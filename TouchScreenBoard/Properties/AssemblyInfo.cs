0f23fb0d120201e017e268d80142fedac1f70198
formula123 <zuzelian123456@gmail.com>
2017-04-19 14:14:28 +0800

    Add nuget item

--- TouchScreenBoard/Properties/AssemblyInfo.cs
+++ TouchScreenBoard/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TouchScreenBoard")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Simple TouchScreen Keyboard")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TouchScreenBoard")]
